255568f3c631ca2115dbea913ae2070581db400b
Andreea Zlei <zleiandreea@gmail.com>
2026-04-06 17:32:21 +0200

    chore: make login and register endpoints public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,19 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from 'src/users/dto/login.dto';
 import { UserDto } from 'src/users/dto/create-user.dto';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {};
 
+    @Public()
     @Post('/login')
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto.email, loginDto.password);
     }
 
+    @Public()
     @Post('/register')
     register(@Body() userDto: UserDto) {
         return this.authService.register(userDto.email, userDto.password, userDto.name);
     }
-
 }
